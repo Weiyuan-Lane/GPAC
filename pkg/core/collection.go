04e03c6b82fce5d397a38ee6680f573fe4c95f84
Weiyuan Liu <weiyuan.lane@gmail.com>
2021-01-25 00:59:00 +0800

    [Feature] Periodic check in progress

--- pkg/core/collection.go
+++ pkg/core/collection.go
@@ -3,5 +3,17 @@ package core
 // type MultipleRetrievalFunc func(keyList []string) (interface{}, error)
 
 // func (p *PageAwareCache) Items(subjectList interface{}, keyList []string, retrieveWith MultipleRetrievalFunc) error {
+// 	itemCacheKeyList := make([]string, len(keyList))
+// 	for i, key := range keyList {
+// 		itemCacheKeyList[i] = p.createItemFullCacheKey(key)
+// 	}
 
+// 	for _, cacheKey := range itemCacheKeyList {
+
+// 	}
+
+// 	cachePayload, err := p.cacheClient.Get(cacheKey)
+// 	if err != nil {
+// 		return err
+// 	}
 // }
