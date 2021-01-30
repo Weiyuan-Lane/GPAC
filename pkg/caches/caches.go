7e2d74838c6cc6567bd9214047fbce26904de610
Weiyuan Liu <weiyuan.lane@gmail.com>
2021-01-30 15:35:13 +0800

    [Feature] Add multiple cache wrapper

--- pkg/caches/caches.go
+++ pkg/caches/caches.go
@@ -2,6 +2,8 @@ package caches
 
 type CacheClient interface {
 	Get(key string) (*string, error)
+	MultipleGet(keys []string) (map[string]string, error)
 	Set(key, val string, ttl int) error
+	MultipleSet(valMap map[string]string, ttl int) error
 	Delete(key string) error
 }
