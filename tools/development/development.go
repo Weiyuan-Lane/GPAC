5bf941c7011de12b2272fb24563bb4882a6174c0
Weiyuan Liu <weiyuan.lane@gmail.com>
2021-02-17 16:44:28 +0800

    [Feature] Fix page logic

--- tools/development/development.go
+++ tools/development/development.go
@@ -14,6 +14,12 @@ type TestStruct struct {
 	Kind string
 }
 
+type TestStructPage struct {
+	HasNextPage     bool
+	HasPreviousPage bool
+	Data            []TestStruct
+}
+
 func main() {
 	testRedis()
 	// testLocalMap()
@@ -37,7 +43,8 @@ func testRedis() {
 
 	// testSimpleItem(gpacWrapperClient)
 	// testItem(gpacWrapperClient)
-	testCollection(gpacWrapperClient)
+	// testCollection(gpacWrapperClient)
+	testPage(gpacWrapperClient)
 }
 
 func testLocalMap() {
@@ -158,4 +165,83 @@ func testCollection(gpacWrapperClient *gpac.PageAwareCache) {
 		}, nil
 	}, []string{"one", "two"})
 	fmt.Println("Retrieved called item as:", itemMap, err)
+}
+
+func testPage(gpacWrapperClient *gpac.PageAwareCache) {
+	page := TestStructPage{}
+	retrieveItemsFrom := func(pageSubject interface{}) ([]interface{}, error) {
+		pagePtr := pageSubject.(*TestStructPage)
+		data := make([]interface{}, len(pagePtr.Data))
+		for i, item := range pagePtr.Data {
+			data[i] = item
+		}
+
+		return data, nil
+	}
+	retrieveKeyFrom := func(subject interface{}) (string, error) {
+		item := subject.(TestStruct)
+		return item.Kind, nil
+	}
+
+	fmt.Println("Retrieving page")
+
+	err := gpacWrapperClient.Page(&page, func(keyArgs ...gpac.ArgReference) (interface{}, error) {
+		return &TestStructPage{
+			HasNextPage:     true,
+			HasPreviousPage: false,
+			Data: []TestStruct{
+				0: {"stuff 10"},
+				1: {"stuff 11"},
+			},
+		}, nil
+	}, retrieveItemsFrom, retrieveKeyFrom,
+		gpac.NewArgReference("num", 1),
+		gpac.NewArgReference("str", "2"),
+		gpac.NewArgReference("rand", "three"),
+	)
+	fmt.Println("Retrieved called page as:", page, err)
+
+	err = gpacWrapperClient.Page(&page, func(keyArgs ...gpac.ArgReference) (interface{}, error) {
+		return &TestStructPage{
+			HasNextPage:     false,
+			HasPreviousPage: true,
+			Data: []TestStruct{
+				0: {"stuff 15"},
+				1: {"stuff 16"},
+			},
+		}, nil
+	}, retrieveItemsFrom, retrieveKeyFrom,
+		gpac.NewArgReference("num", 1),
+		gpac.NewArgReference("str", "2"),
+		gpac.NewArgReference("rand", "three"),
+	)
+	fmt.Println("Retrieved called page (without calling direct but from cache) as:", page, err)
+
+	var item TestStruct
+	_ = gpacWrapperClient.SimpleItem(&item, func(key string) (interface{}, error) {
+		fmt.Println("Called")
+		return &TestStruct{"stuff 19991"}, nil
+	}, "stuff 10")
+	fmt.Println("Retrieved called item as:", item)
+
+	fmt.Println("Waiting 10 seconds for TTL to lapse")
+	time.Sleep(11 * time.Second)
+	fmt.Println("Retrieving item again")
+
+	err = gpacWrapperClient.Page(&page, func(keyArgs ...gpac.ArgReference) (interface{}, error) {
+		return &TestStructPage{
+			HasNextPage:     false,
+			HasPreviousPage: true,
+			Data: []TestStruct{
+				0: {"stuff 17"},
+				1: {"stuff 18"},
+			},
+		}, nil
+	}, retrieveItemsFrom, retrieveKeyFrom,
+		gpac.NewArgReference("num", 1),
+		gpac.NewArgReference("str", "2"),
+		gpac.NewArgReference("rand", "three"),
+	)
+	fmt.Println("Retrieved called page as:", page, err)
+
 }
